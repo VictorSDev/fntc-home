311506ac3a631b689f3c2e1bead8c8621e10b64f
Victor Schmidt <victorh.schmidtm@gmail.com>
2023-05-02 12:35:02 -0300

    Fix Responsive

--- src/app/components/header/HeaderIndex.tsx
+++ src/app/components/header/HeaderIndex.tsx
@@ -10,7 +10,7 @@ function HeaderIndex() {
   return (
     <div className="w-full flex flex-row justify-between items-center py-5 px-14">
       <Icon />
-      <div className="w-auto flex flex-row xl:space-x-40 lg:space-x-28 md:space-x-12 sm:space-x-5">
+      <div className="w-auto flex flex-row 2xl:space-x-60 xl:space-x-40 lg:space-x-28 md:space-x-12 sm:space-x-8 space-x-5">
         <Solutions />
         <Tax />
         <Help />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ function Home() {
   return (
     <main>
       <HeaderIndex />
-      <div className="w-full flex flex-row justify-between items-center px-40 pt-16">
-        <div className="w-1/2 space-y-20">
+      <div className="w-full flex flex-row justify-between items-center px-20 pt-16">
+        <div className="w-auto space-y-20">
           <BodyIndex />
           <ButtonIndex />
         </div>
         <WomanHome />
       </div>
-      <div className="w-full flex flex-row justify-between items-center px-40 pt-20 md:space-x-12 lg:space-x-24">
+      <div className="w-full grid gap-8 2xl:grid-cols-4 lg:grid-cols-2 justify-between items-center px-32 pt-20">
         <HighTaxIndex />
         <PerformanceIndex />
         <IntegrationIndex />
